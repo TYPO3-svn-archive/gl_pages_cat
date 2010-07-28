3debdfcc6e4d83f07575aaaa0f79891136c746f9
mrego <mrego@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-28 08:21:11 +0000

    * ext_tables.php: [Bug #7035] Fixed bug displaying category tree with 	tt_news 3.0.1 (thanks to Jason Steiner for reporting).
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/gl_pages_cat/trunk@36124 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -15,37 +15,13 @@ $tempColumns = Array (
 		'config' => Array (
 			'type' => 'select',
 			'form_type' => 'user',
-			'userFunc' => 'tx_ttnews_treeview->displayCategoryTree',
+			'userFunc' => 'tx_ttnews_TCAform_selectTree->renderCategoryFields',
 			'treeView' => 1,
 			'foreign_table' => 'tt_news_cat',
-			'size' => 3,
 			'autoSizeMax' => $confArr['categoryTreeHeigth'],
 			'minitems' => $confArr['requireCategories'] ? 1 : 0,
 			'maxitems' => 500,
 			'MM' => 'pages_cat_mm',
-			'wizards' => Array(
-				'_PADDING' => 2,
-				'_VERTICAL' => 1,
-				'add' => Array(
-					'type' => 'script',
-					'title' => 'LLL:EXT:tt_news/locallang_tca.php:tt_news.createNewCategory',
-					'icon' => 'EXT:tt_news/res/add_cat.gif',
-					'params' => Array(
-						'table'=>'tt_news_cat',
-						'pid' => $sPid,
-						'setValue' => 'set'
-					),
-					'script' => 'wizard_add.php',
-				),
-				'edit' => Array(
-					'type' => 'popup',
-					'title' => 'LLL:EXT:tt_news/locallang_tca.php:tt_news.editCategory',
-					'script' => 'wizard_edit.php',
-					'popup_onlyOpenIfSelected' => 1,
-					'icon' => 'edit2.gif',
-					'JSopenParams' => 'height=350,width=580,status=0,menubar=0,scrollbars=1',
-				),
-			),
 		),
 	),
 );
